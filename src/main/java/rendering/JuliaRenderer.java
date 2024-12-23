ef93a030a393c09d7da8e137be0fbe7ca7c88c85
westward-ho <achim.quellmalz@freenet.de>
2024-12-23 12:53:30 +0000

    modified:   src/main/java/rendering/JuliaRenderer.java

--- src/main/java/rendering/JuliaRenderer.java
+++ src/main/java/rendering/JuliaRenderer.java
@@ -1,4 +1,12 @@
 package rendering;
+
+import utils.ComplexNumber;
+import utils.ColourMapper;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.paint.Color;
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+
 public class JuliaRenderer {
     
 }
